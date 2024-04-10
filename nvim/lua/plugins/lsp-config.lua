5c11d934ad8ac6a6815dbde797fd52b98a2b8729
thomasdaly <tdaly91@gmail.com>
2024-04-10 11:09:52 +1000

    changed alpha loading screen. Error with recent files on macos, file cd issue.

--- nvim/lua/plugins/lsp-config.lua
+++ nvim/lua/plugins/lsp-config.lua
@@ -28,10 +28,27 @@ return {
       lspconfig.html.setup({
         capabilities = capabilities
       })
+      lspconfig.black.setup({
+        capabilities = capabilities
+      })
       lspconfig.lua_ls.setup({
         capabilities = capabilities
       })
-
+      lspconfig.phpactor.setup({
+  cmd = { "phpactor", "language-server" },
+  filetypes = { "php", "blade" },
+  root_dir = lspconfig.util.root_pattern("composer.json", ".git"),
+  init_options = {
+    ["language_server_worse_reflection.inlay_hints.enable"] = true,
+    ["language_server_worse_reflection.inlay_hints.params"] = true,
+    -- ["language_server_worse_reflection.inlay_hints.types"] = true,
+    ["language_server_configuration.auto_config"] = false,
+    ["code_transform.import_globals"] = true,
+    ["language_server_phpstan.enabled"] = true,
+    ["language_server_phpstan.level"] = 7,
+    ["language_server_phpstan.bin"] = "phpstan",
+  },
+})
       vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
       vim.keymap.set("n", "<leader>gd", vim.lsp.buf.definition, {})
       vim.keymap.set("n", "<leader>gr", vim.lsp.buf.references, {})
